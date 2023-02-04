5d71b99b93968a07000c7942f728877fd5c2513e
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-02-04 22:34:05 +0200

    Get & Create Listings

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import PrivateRoute from './components/PrivateRoute'
 
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
+import Category from './pages/Category'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -19,12 +21,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />}></Route>
           <Route path='/offers' element={<Offers />}></Route>
+          <Route path='/category/:categoryName' element={<Category />}></Route>
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />}></Route>
           </Route>
           <Route path='/sign-in' element={<SignIn />}></Route>
           <Route path='/sign-up' element={<SignUp />}></Route>
           <Route path='/forgot-password' element={<ForgotPassword />}></Route>
+          <Route path='/create-listing' element={<CreateListing />}></Route>
         </Routes>
         <Navbar />
       </Router>
